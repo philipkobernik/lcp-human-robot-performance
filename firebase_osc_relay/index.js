1179f3af9d2ea97c2d217b10552dac023d7fbbcf
Philip Kobernik <ptk921@gmail.com>
2020-12-01 12:25:52 -0800

    configure firebase inline for convenience

--- firebase_osc_relay/index.js
+++ firebase_osc_relay/index.js
@@ -4,14 +4,23 @@ var firebase = require("firebase");
 var osc = require('osc');
 
 // Initialize firebase
+//
+var FIREBASE_DATABASE_URL="https://lcp-human-robot-performance.firebaseio.com";
+var FIREBASE_AUTH_DOMAIN="lcp-human-robot-performance.firebaseapp.com";
+var FIREBASE_API_KEY="AIzaSyDivUHq7yQ0NFhKJWU5uO4x_VK6LYHsiJo";
+var FIREBASE_PROJECT_ID="lcp-human-robot-performance";
+var FIREBASE_STORAGE_BUCKET="lcp-human-robot-performance.appspot.com";
+var FIREBASE_MESSAGING_SENDER_ID="824569421218";
+var FIREBASE_APP_ID="1:824569421218:web:bee37ca081137d3bf566fb";
+
 var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID
   };
 
   let userId = process.argv.length > 2 ? process.argv[2] : 'default';
